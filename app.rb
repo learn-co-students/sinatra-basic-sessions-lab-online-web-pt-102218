1098ee493a7691200e57c021d8a36a982ae3a573
Johnathon Wood <jaywood128@gmail.com>
2019-02-13 21:39:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+enable :sessions
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+
+    @item = session.merge!("item" => "socks")
+
+    erb :checkout
+  end
 end
