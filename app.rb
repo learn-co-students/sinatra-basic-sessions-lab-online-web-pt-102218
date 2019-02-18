79c42ac596a78852693b09c33a2ddf2c09d50b54
Linda Chang <lindachang186@gmail.com>
2019-02-18 01:03:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+  end
+
 end
