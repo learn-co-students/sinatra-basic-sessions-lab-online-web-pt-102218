e27e09c0db9c04a25bfc4a788a421c5f35016eea
Brandon Teich <brandonteich123@gmail.com>
2019-02-22 05:55:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do
+    @session=session
+    erb :index
+  end
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  post "/checkout" do
+    puts params
+    @item = params["item"]
+    session["item"]=params["item"]
+    erb :cart
+  end
+  
 end
